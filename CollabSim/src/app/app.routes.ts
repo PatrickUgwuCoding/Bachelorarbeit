9f919f2a0b2a3dfd14572ecc01d89266b3b31fd6
Patrick Ugwu <patrick.ugwu@web.de>
2024-05-01 16:14:27 +0200

    updated routes

--- CollabSim/src/app/app.routes.ts
+++ CollabSim/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SimulationComponent } from './simulation/simulation.component';
+import { XMLComponent } from './xml/xml.component';
+import { CPPComponent } from './cpp/cpp.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Sim',component: SimulationComponent},
+    {path:'Xml',component: XMLComponent},
+    {path:'Cpp',component: CPPComponent},
+    {path:'', redirectTo: 'Xml', pathMatch: 'full'}
+];
